00cf0716c8d1de7c4f91df6887d298ddfd690897
ᴀsᴛʀɪᴏx <71863236+Astriox@users.noreply.github.com>
2023-05-04 17:46:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '7822571'))
+API_HASH = environ.get('API_HASH', '067329e70bfc5b3022f77080703da788')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '5493832202'))
+OWNER = int(environ.get('OWNER', '1908563535'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', True))
